413022c89cbc60f2140483b34557153e08f7bf2b
Carolina <carolina.peseca@gmail.com>
2025-05-06 10:57:31 +0200

    vite change

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: "./",
   root: './src',
   build: {
     // Specify the output directory for the build. This path is relative to your project root.
